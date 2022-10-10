8d4f94d01f9466a97641ee1c98755c5423bb9e3a
knohnter <115503386+knohnter@users.noreply.github.com>
2022-10-10 22:38:27 +0200

    Update and rename main.js to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let btn = GetElementByID('btn');
+let username = GetElementByID('name');
+let output = GetElementByID('output');
+
+btn.addEventListener('click', function() {
+    if(name == null){
+      
+    }else{
+        output = username.textContent; 
+    }
+}
