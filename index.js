e3a49d0ccb5176e8db77ef0477839d79e1d43667
knohnter <felix_aidam@outlook.de>
2022-10-13 13:38:18 +0200

    laying some groundwork, added node.js and jquery

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let btn = GetElementByID('btn');
-let username = GetElementByID('name');
-let output = GetElementByID('output');
+let btn = document.getElementById('btn');
+let username = document.getElementById('name');
+let output = document.getElementById('output');
+
 
 btn.addEventListener('click', function() {
-    output.innerHTML = username.textContent; 
+    output.innerHTML = username.value; 
+    $("#output").load("test.txt");
 });
